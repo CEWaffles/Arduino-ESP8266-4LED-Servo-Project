d5096b862b80f1783d995d17d86bd917af1574be
CEWaffles <webmonkey78@gmail.com>
2021-02-02 10:51:46 -0800

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "[YourSSI_Name]"
+#define SECRET_PASS "[YourSSID_Key]"
+#define TWITCHBOT_OAUTH_TOKEN "oauth:[See ino file for info]"
+#define SECRET_CHANNELID ""
+#define SECRET_TTS "TRUE"
